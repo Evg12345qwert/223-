0d0db671f15865340b3534aef8520a2f36f31269
Evg12345qwert <137744072+Evg12345qwert@users.noreply.github.com>
2024-09-02 23:11:00 +0300

    Add files via upload

---
+++ ту ду приложение/app.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
+from forms import LoginForm, RegisterForm, TaskForm
+from models import db, User, Task
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful! You can now log in.')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Login failed. Check your username and password.')
+    return render_template('login.html', form=form)
+
+@app.route('/')
+@login_required
+def index():
+    tasks = Task.query.filter_by(user_id=current_user.id).all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add_task():
+    title = request.form.get('task')
+    if title:
+        new_task = Task(title=title, user_id=current_user.id)
+        db.session.add(new_task)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
+@login_required
+def edit_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    if task.user_id != current_user.id:
+        return redirect(url_for('index'))
+    
+    form = TaskForm()
+    if form.validate_on_submit():
+        task.title = form.title.data
+        db.session.commit()
+        return redirect(url_for('index'))
+    
+    form.title.data = task.title
+    return render_template('edit.html', form=form)
+
+@app.route('/delete/<int:task_id>')
+@login_required
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    if task.user_id == current_user.id:
+        db.session.delete(task)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
